8790c0a1f353db8ba1bdc6560692f390ad52be49
bonxia <dowhatidream@gmail.com>
2025-11-27 19:51:46 +0900

    [이승연] main파일에 리뷰요약 router 경로 추가

--- app/main.py
+++ app/main.py
@@ -5,7 +5,9 @@ from dotenv import load_dotenv
 # from config.database.session import Base, engine
 # from social_oauth.adapter.input.web.google_oauth2_router import authentication_router
 # from account.adapter.input.web.account_router import account_router
-from naver.adapter.input.web.naver_router import naver_router
+from naver.adapter.input.naver_router import naver_router
+from review.adapter.input.web.review_router import review_router
+
 
 load_dotenv()
 
@@ -30,6 +32,7 @@ app.add_middleware(
 # app.include_router(authentication_router, prefix="/authentication")
 # app.include_router(account_router, prefix="/account")
 app.include_router(naver_router, prefix="/naver")
+app.include_router(review_router, prefix="/reviews")
 
 # 앱 실행
 if __name__ == "__main__":
